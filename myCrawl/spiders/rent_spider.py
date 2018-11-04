405766d58d9244cddf0c67544cce6022aabaa55d
I-HSIEN HUANG <isytkr34@gmail.com>
2018-11-04 01:11:20 -0800

    version for crawl all datas from rent.com

--- myCrawl/spiders/rent_spider.py
+++ myCrawl/spiders/rent_spider.py
@@ -11,18 +11,17 @@ from time import sleep
 class BaiduSpider(CrawlSpider):
 	name = "rent"
 	allowed_domains = ["rent.com"]
-	df = pd.read_csv("../../state_city.csv")
-	datas = df['California']
+	df = pd.read_csv("state_city.csv")
 	urls = []
-	for data in datas:
-		#print(data)
-		url = 'https://www.rent.com/california/'+str(data)+'/apartments_condos_houses_townhouses?page=2'
-		urls.append(url)
-	start_urls = [val for val in urls]
-	#start_urls = [r'https://www.rent.com/california/riverside/apartments_condos_houses_townhouses?page=2',
-	#			  r'https://www.rent.com/california/redlands/apartments_condos_houses_townhouses?page=2']
-				  #'https://www.rent.com/california/riverside-apartments/berkdale-apartments-4-477789',
-				  #'https://www.rent.com/california/riverside-apartments/cranford-court-4-468020']
+	for col in df.columns:
+		datas = df[col]
+		for data in datas:
+			data = str(data).replace(" ","-")
+			col = str(col).replace(" ","-")	
+			url = 'https://www.rent.com/'+str(col)+'/'+str(data)+'/apartments_condos_houses_townhouses?page=2'
+			urls.append(url)
+	all_cities = urls
+	start_urls = ['https://www.rent.com/Colorado/Wheat-Ridge/apartments_condos_houses_townhouses?page=2']
 	rules = [Rule(LinkExtractor(
 				#allow=(r'\/california/riverside-apartments\?page[2-9]'),
 				canonicalize=True,
@@ -39,38 +38,63 @@ class BaiduSpider(CrawlSpider):
 		Rule(LinkExtractor(allow = (r'\?page=[2-9]',)) ,callback = 'parse',follow=True)
 		]
 	'''
-	urls = set()
+	seen_urls = set()	
 	page =1
 	clock = 0
-	#dic_url = set() 	
-	def parse(self,response):
-		#dic_url.add(response.url)
-		print ("url is ",response.url)
+	def parse_city(self,response):
+		#print ("parse city url is ",response.url)
+		
 		links = LinkExtractor(canonicalize=True, unique=True).extract_links(response)
 		flag = False
 		for link in links:
 			obj = re.match(r'https://www\.rent\.com/.+/.*/.+[0-9]$',link.url,re.M|re.I)
 			review = re.match(r'https://www\.rent\.com/california/riverside-apartments/.+review.+',link.url,re.M|re.I)	# review should not in link
-			if obj and "reviews" not in link.url :
+			if obj and "reviews" not in link.url and '/tel' not in link.url and "page" not in link.url and link.url not in self.seen_urls:
 				self.clock+=1
-				flag = True
 				#print(self.clock)
-				if self.clock <10000 and '/tel' not in link.url and link.url not in self.urls:
+				if self.clock <100000:
 					print(self.clock,link.url)
-					self.urls.add(link.url)
+					flag = True
+					self.seen_urls.add(link.url)
 					yield scrapy.Request(link.url,callback=self.parse_data,dont_filter=True)
 		next_url = response.url.split("=")
 		next_url,page = next_url[0],next_url[1]
 		next_url = next_url +"="+str(int(page)+1)	
 		print ("next url",next_url)	
-		if flag:
+		if flag and int(page) <70:
+			yield scrapy.Request(next_url,callback=self.parse_city,dont_filter=True)
+			#print (len(dic_url))
+
+	def parse(self,response):
+		for url in  self.urls:
+			yield scrapy.Request(url,callback=self.parse_city,dont_filter=True)
+		'''
+		links = LinkExtractor(canonicalize=True, unique=True).extract_links(response)
+		flag = False
+		for link in links:
+			obj = re.match(r'https://www\.rent\.com/[a-zA-z\-]+/.*/.+[0-9]$',link.url,re.M|re.I)
+			review = re.match(r'https://www\.rent\.com/california/riverside-apartments/.+review.+',link.url,re.M|re.I)	# review should not in link
+			if obj and "reviews" not in link.url :
+				#self.clock+=1
+				flag = True
+				#print(self.clock)
+				if self.clock <10000 and '/tel' not in link.url and link.url not in self.seen_urls:
+					print(self.clock,link.url)
+					self.seen_urls.add(link.url)
+					yield scrapy.Request(link.url,callback=self.parse_data,dont_filter=True)
+		next_url = response.url.split("=")
+		next_url,page = next_url[0],next_url[1]
+		next_url = next_url +"="+str(int(page)+1)	
+		print ("next url",next_url)	
+		if flag and page <100:
 			yield scrapy.Request(next_url,callback=self.parse,dont_filter=True)
-			#print (len(dic_url))	
+			#print (len(dic_url))
+		'''
 	def parse_data(self,response):
 		#print("start process data)",response.url) 
 		#if self.clock%1000==0:
 		#	sleep(1)
-		
+		print (response.url)	
 		item = FirstDemoItem()
 		item['url_from'] = response.url
 		item['title'] = response.xpath('/html/head/title/text()').extract()
@@ -91,7 +115,7 @@ class BaiduSpider(CrawlSpider):
 		for div in divs:
 			#print("round:", round_num)
 			#round_num +=1		
-			item['phone'] = div.xpath(".//span[@class='pdp-heading-meta-phone hidden-xs']/a/@href").extract()
+			#item['phone'] = div.xpath(".//span[@class='pdp-heading-meta-phone hidden-xs']/a/@href").extract()
 			item['price'] = div.xpath(".//span[@class='pdp-heading-meta-rent bold']/text()").extract()    
 			item['beds']  = div.xpath(".//span[@class='pdp-heading-meta-beds']/text()").extract()   
 			item['pet'] = div.xpath(".//span[@class='pdp-heading-meta-pets']/text()").extract()    
@@ -106,9 +130,30 @@ class BaiduSpider(CrawlSpider):
 			data_size =  (size.xpath(".//text()").extract())[0].replace('\xa0',"")
 			res =res + data_style+" "+data_price+ " "+data_size +" | "
 		item['house_plan']  = res
-		#print ("---end working for home plans")
-		parking_info = response.xpath("//div[@class='amenity-grid clearfix']")
+		#parking_info = response.xpath("//div[@class='amenity-grid clearfix']")
+		parking_info = response.xpath("//div[@class='amenity-group-content']")
+		#amenity-group-content
+		block_info = response.xpath("//div[@class='amenity-group']")
+		print ("start testing")
+		attribute = [] # stroe the list of attribute like pet, part and so on
+		for data in (block_info):
+			#name = data.xpath(".//div[@class='amenity-group']/test()").extract()
+			name = data.xpath(".//h3/text()").extract()
+			content = data.xpath(".//div[@class='amenity-group-content']/text()").extract()
+			content2 = data.xpath(".//span[@class='sprite-text']/text()").extract()
+			print ("@@@@@@@@ ",name,content,content2)
+			#print (name)
+			attribute.append(name)
+		#print ("end testing")	
+		print ("start testing")
 		for index,park in enumerate(parking_info):
+			attribute_name = attribute[index] #if attribute[index][-1] ==":" else attribute[index]
+			attribute_name = str(attribute_name).replace(":']","")
+			attribute_name = str(attribute_name).replace("['","")
+			#print (str(attribute_name))
+			item[attribute_name] = park.xpath(".//span[@class='sprite-text']/text()").extract()
+			print (attribute_name,item[attribute_name])
+			'''
 			if index ==0:
 				item['Kitchen'] = park.xpath(".//span[@class='sprite-text']/text()").extract()
 			elif index ==1:
@@ -122,5 +167,7 @@ class BaiduSpider(CrawlSpider):
 				item['Features'] = park.xpath(".//span[@class='sprite-text']/text()").extract()
 			elif index ==5:
 				item['Community'] = park.xpath(".//span[@class='sprite-text']/text()").extract()
+			'''
+		print ("end testin")
 		yield item		
 	 	
